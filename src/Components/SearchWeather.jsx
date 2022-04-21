cc6a96433ffaff74f66fed93cefa68b75618f02a
Hariskhan2 <89725754+Hariskhan2@users.noreply.github.com>
2022-04-21 18:22:31 +0500

    Creation of card of Weather component

---
+++ src/Components/SearchWeather.jsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from "react";
+
+export default function SearchWeather() {
+  const [search, setSearch] = useState("Kasur");
+  const [data, setData] = useState([]);
+  const [input, setinput] = useState("");
+  const change = (e) => {
+    setinput(e.target.value);
+  };
+  let componentMounted = true;
+  useEffect(() => {
+    const fetchweather = async () => {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=b90cba412a05c40b9fe7e249ad1b73ed`
+      );
+      if (componentMounted) {
+        setData(await response.json());
+        console.log(data);
+      }
+      return () => {
+        componentMounted = false;
+      };
+    };
+    fetchweather();
+  }, [search]);
+  let emoji = null;
+  if (typeof data.main != "undefined") {
+    if (data.weather[0].main == "Clouds") {
+      emoji = "fa-cloud";
+    } else if (data.weather[0].main == "Thunderstrom") {
+      emoji = "fa-bolt";
+    } else if (data.weather[0].main == "Drizzle") {
+      emoji = "fa-cloud-rain";
+    } else if (data.weather[0].main == "Rain") {
+      emoji = "fa-cloud-shower-heavy";
+    } else if (data.weather[0].main == "Snow") {
+      emoji = "fa-snow-flake";
+    } else {
+      emoji = "fa-smog";
+    }
+  } else {
+    return <div>...Loading</div>;
+  }
+  //to convert temperature f to C
+  let temp = (data.main.temp - 273.15).toFixed(2);
+  let temp_min = (data.main.temp_min - 273.15).toFixed(2);
+  let temp_max = (data.main.temp_max - 273.15).toFixed(2);
+  //date
+  let d = new Date();
+  let date = d.getDate();
+  let year = d.getFullYear();
+  let month = d.toLocaleString("default", { month: "long" });
+  let day = d.toLocaleString("default", { weekday: "long" });
+  //time
+  let time = d.toLocaleString("default", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearch(input);
+  };
+  return (
+    <div>
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-md-4">
+            <div class="card text-white text-center border-0">
+              <img
+                src={`https://source.unsplash.com/random/600×900/?${data.weather[0].main}`}
+                class="card-img"
+                alt="..."
+              />
+              <div class="card-img-overlay">
+                <form onSubmit={handleSubmit}>
+                  <div class="input-group mb-4 w-75 mx-auto">
+                    <input
+                      type="search"
+                      class="form-control"
+                      placeholder="Search City"
+                      aria-label="Search City"
+                      aria-describedby="basic-addon2"
+                      name="search"
+                      value={input}
+                      onChange={change}
+                      required
+                    />
+                    <button
+                      type="submit"
+                      class="input-group-text"
+                      id="basic-addon2"
+                    >
+                      <i className="fas fa-search"></i>
+                    </button>
+                  </div>
+                </form>
+                <div className="bg-dark bg-opacity-50 py-3">
+                  <h2 class="card-title">{data.name}</h2>
+                  <p class="card-text lead">
+                    {day}, {month} {date}, {year}
+                    <br />
+                    {time}
+                  </p>
+                  <hr />
+                  <i className={`fas ${emoji} fa-4x`}></i>
+                  <h1 className="fw-bolder mb-5">{temp} &deg;C</h1>
+                  <p className="lead fw-bolder mb-0">{data.weather[0].main}</p>
+                  <p className="lead">
+                    {temp_min} &deg;C | {temp_max} &deg;C
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+//https://source.unsplash.com/1600×900/?nature,water
